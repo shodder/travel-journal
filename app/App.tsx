3d8060f6ba428c87aa023afb237c4e5d559045aa
Simon Hodder <2083432+shodder@users.noreply.github.com>
2025-08-31 17:10:28 +0100

    Creating components from data

--- app/App.tsx
+++ app/App.tsx
@@ -2,14 +2,97 @@
 import Navbar from "./components/Navbar"
 import Entry from "./components/Entry"
 
+// Import images as static assets to help when building/deploying
+// import MountFuji from "/public/images/mount-fuji.jpg"
+
+// load the data straight into a JS object to use
+import journalData from "/data"
+
+
+/**
+ * Challenge:
+ * - import the array of data from data.js
+ * - map over the array to create an <Entry /> component
+ *   for every item in the data array.
+ * - display the array of Entry components in place of the current
+ *   hard-coded <Entry /> instance.
+ */
+
 export default function App() {
+  
+  // Map the incoming data to entry jsx elements
+  const journalElements = journalData.map(
+    (entry) => {
+      return (
+        <Entry 
+          // image={
+          //     {
+          //       src: entry.img.src,
+          //       alt: entry.img.alt
+          //     }
+          //   }
+            image={entry.img} // can do this rather than above as objects match
+            country={entry.country}
+            googleMapsLink={entry.googleMapsLink}
+            title={entry.title}
+            dates={entry.dates}
+            info={entry.text}
+            // For React to track items in a list of elements
+            // each item MUST have a unique key assigned to "key" prop
+            // Need this to add/remove items fro mthe element list
+            // In this example we have IDs for this!
+            key={entry.id}
+        />
+      )
+    }
+  )
+
   return (
     <>
       <Navbar />
       <main className="entries-container" >
-        <Entry />
+        {journalElements}
       </main>
     </>
   )
 }
+
+
+
+
+ /* Challenge: pass props to the Entry component to display
+ * its data. See the `data.md` file for each prop name and its
+ * value.
+ * 
+ * Then on the Entry component, receive and display the values
+ * for those props. In the end, the page should look the same
+ * as it does now, but without all the hard-coded data in the
+ * component
+ */
+
+
+
+// export default function App() {
+//   console.log("Simon")
+//   return (
+//     <>
+//       <Navbar />
+//       <main className="entries-container" >
+//         <Entry 
+//           image={
+//             {
+//               src: MountFuji,
+//               alt:"Mount Fuji"
+//             }
+//           }
+//           country="Japan"
+//           googleMapsLink="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu"
+//           title="Mount Fuji"
+//           dates="12 Jan, 2021 - 24 Jan, 2021"
+//           info="Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+//         />
+//       </main>
+//     </>
+//   )
+// }
 
--- app/components/Entry.tsx
+++ app/components/Entry.tsx
@@ -1,19 +1,22 @@
 
-export default function Entry() {
+export default function Entry(props) {
     return (
         <article className="journal-article-container">
             <div className="journal-main-image-container">
-                <img className="journal-main-image" src="/public/images/mount-fuji.jpg" alt="Mount Fuji"/>
+                <img className="journal-main-image" 
+                src={props.image.src} 
+                alt={props.image.alt}/>
             </div>
             <div className="journal-entry-container">
-                <img className="journal-marker-image"src="/public/images/marker.png" alt="Map marker" />
-                <span className="journal-country">Japan</span>
-                <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">
-                View on Google Maps
-                </a>
-                <h2 className="journal-title">Mount Fuji</h2>
-                <p className="journal-date">12 Jan, 2021 - 24 Jan, 2021</p>
-                <p className="journal-text">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                <img className="journal-marker-image"
+                    src="/public/images/marker.png"
+                    alt="Map marker" 
+                />
+                <span className="journal-country">{props.country}</span>
+                <a href={props.googleMapsLink}>View on Google Maps</a>
+                <h2 className="journal-title">{props.title}</h2>
+                <p className="journal-date">{props.dates}</p>
+                <p className="journal-text">{props.info}</p>
             </div>
         </article>
         
